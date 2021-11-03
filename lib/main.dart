c1b87270c6ee4a8bf1c1d5e9c1d1ffd7c61bc52c
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2021-11-03 16:36:56 +0500

    added icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:ceo_transport/home_page.dart';
 import 'package:ceo_transport/screens/auth/login.dart';
 import 'package:flutter/material.dart';
 
@@ -21,15 +20,6 @@ class MyApp extends StatelessWidget {
             subtitle1: TextStyle(fontSize: 18),
             subtitle2: TextStyle(fontSize: 20),
             bodyText1: TextStyle(fontSize: 20, color: Colors.black),
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
           ),
           primaryColor: Colors.amber),
       home: LoginScreen(),
