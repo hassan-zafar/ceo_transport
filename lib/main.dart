10a42d17e7e1c90308e9d08325f9b333a6e83592
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2021-11-09 20:34:49 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    bool isLoggedIn = true;
     return MaterialApp(
       title: 'CEO Transport',
       theme: ThemeData(
